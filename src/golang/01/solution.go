775ce4dfe2385130445bdcbacead8a773e43b0c8
dhorn <dhorn@home.it>
2024-12-02 07:53:55 +0000

    .

--- src/golang/01/solution.go
+++ src/golang/01/solution.go
@@ -1,18 +1,22 @@
 package main
 
-import example/hello/src/golang/aocutils
+import (
+	"example/hello/src/golang/aocutils"
+	"fmt"
+	"regexp"
+)
 
 const inputFile = "input_test.txt"
 
 func part1() {
-	inputData.ReadInput(inputFile)
+	inputData := aocutils.ReadInput(inputFile)
 	r, _ := regexp.Compile(`\d+`)
 
 	for _, line := range inputData {
 		fmt.Println(line)
 		matches := r.FindAllString(line, -1)
+		fmt.Println(matches)
 	}
-
 
 }
 
