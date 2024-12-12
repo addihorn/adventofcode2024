5f58e716aabab751f94fe3b7d53b89b29e02469c
addihorn <danhorn87@googlemail.com>
2024-12-12 16:21:06 +0000

    day 12
    solve both parts in one go

--- src/golang/12/solution.go
+++ src/golang/12/solution.go
@@ -11,7 +11,7 @@ import (
 )
 
 const debug = false
-const inputFile = "bug.txt"
+const inputFile = "input.txt"
 
 var inputData []string
 
@@ -47,65 +47,6 @@ func initializePuzzle() {
 			gardenData[Cell{x, y}] = &CellData{plant: plant, original: plant, left: false, right: false, up: false, down: false}
 		}
 	}
-}
-
-func floodGardenPatchFrom(garden map[Cell]rune, startCell Cell) (area, perimeter, sideCount int) {
-
-	perimeter = 0
-	area = 0
-
-	queue := []Cell{startCell}
-	seen := []Cell{}
-	sides := make(map[string]int)
-
-	for len(queue) > 0 {
-		cell := queue[0]
-		x, y := cell[0], cell[1]
-		area++
-		seen = append(seen, cell)
-
-		//delete cell from search tree
-		queue = queue[1:]
-
-		//check neighbors
-		for _, delta_x := range []int{1, -1} {
-			neigh, ok := garden[Cell{x + delta_x, y}]
-			neigSeen := slices.Contains(seen, Cell{x + delta_x, y})
-
-			if !ok || (neigh != garden[cell] && !neigSeen) {
-				perimeter++
-				sides[fmt.Sprintf("X %d %d", x, delta_x)]++
-				continue
-			}
-			if !slices.Contains(queue, Cell{x + delta_x, y}) && !neigSeen {
-				queue = append(queue, Cell{x + delta_x, y})
-			}
-
-		}
-
-		for _, delta_y := range []int{1, -1} {
-
-			neigh, ok := garden[Cell{x, y + delta_y}]
-			neigSeen := slices.Contains(seen, Cell{x, y + delta_y})
-			if !ok || (neigh != garden[cell] && !neigSeen) {
-				perimeter++
-				sides[fmt.Sprintf("Y %d %d", y, delta_y)]++
-				continue
-			}
-			if !slices.Contains(queue, Cell{x, y + delta_y}) && !slices.Contains(seen, Cell{x, y + delta_y}) {
-				queue = append(queue, Cell{x, y + delta_y})
-			}
-		}
-		garden[cell] = '.'
-		if debug {
-			PaintGrid(gridSize, garden)
-		}
-
-	}
-	sideCount = len(sides)
-
-	return
-
 }
 
 func FloodPt2GardenPatchFrom(garden map[Cell]*CellData, startCell Cell) (area, perimeter, edges, corners int) {
@@ -227,6 +168,10 @@ func FloodPt2GardenPatchFrom(garden map[Cell]*CellData, startCell Cell) (area, p
 
 		garden[cell].plant = '.'
 
+		if debug {
+			PaintGrid(gridSize, garden)
+		}
+
 	}
 
 	return
@@ -238,16 +183,23 @@ var cost_pt2 = 0
 
 /* Solve here */
 func solve() {
-	gardenCopy := maps.Clone(garden)
+	gardenCopy := maps.Clone(gardenData)
 
-	for x := gridSize.MinX; x < gridSize.MaxX+1; x++ {
-		for y := gridSize.MinY; y < gridSize.MaxY+1; y++ {
-			plant := string(gardenCopy[Cell{x, y}])
+	for x := gridSize.MinX; x < gridSize.MaxX; x++ {
+		for y := gridSize.MinY; y < gridSize.MaxY; y++ {
+			plant := string(gardenCopy[Cell{x, y}].plant)
 			_ = plant
-			if !(gardenCopy[Cell{x, y}] == 0 || gardenCopy[Cell{x, y}] == '.') {
-				area, perimeter, sides := floodGardenPatchFrom(gardenCopy, Cell{x, y})
+			if !(gardenCopy[Cell{x, y}].plant == 0 || gardenCopy[Cell{x, y}].plant == '.') {
+				area, perimeter, sides, corners := FloodPt2GardenPatchFrom(gardenCopy, Cell{x, y})
+
 				cost_pt1 += area * perimeter
-				cost_pt2 += area * sides
+				//costByEdges += area * sides
+				cost_pt2 += area * corners
+
+				if corners != sides {
+					fmt.Printf("Found some disrepancy while calculating edges on area %s with starting point %d %d\n", string(plant), x, y)
+				}
+
 			}
 		}
 	}
@@ -260,40 +212,16 @@ func part1() {
 
 func part2() {
 
-	gardenCopy := maps.Clone(gardenData)
-	costByFences := 0
-	costByEdges := 0
-	costByCorners := 0
-
-	for x := gridSize.MinX; x < gridSize.MaxX; x++ {
-		for y := gridSize.MinY; y < gridSize.MaxY; y++ {
-			plant := string(gardenCopy[Cell{x, y}].plant)
-			_ = plant
-			if !(gardenCopy[Cell{x, y}].plant == 0 || gardenCopy[Cell{x, y}].plant == '.') {
-				area, perimeter, sides, corners := FloodPt2GardenPatchFrom(gardenCopy, Cell{x, y})
-
-				costByFences += area * perimeter
-				costByEdges += area * sides
-				costByCorners += area * corners
-
-				if corners != sides {
-					fmt.Printf("Found some disrepancy while calculating edges on area %s with starting point %d %d\n", string(plant), x, y)
-				}
-
-			}
-		}
-	}
-
-	fmt.Printf("Solution for part 2: %d %d %d\n", costByFences, costByEdges, costByCorners)
+	fmt.Printf("Solution for part 2: %d\n", cost_pt2)
 }
 
-func PaintGrid(this *aocutils.Gridsize, grid map[Cell]rune) {
+func PaintGrid(this *aocutils.Gridsize, grid map[Cell]*CellData) {
 	screen.Clear()
 	output := ""
 	for y := this.MinY; y <= this.MaxY; y++ {
 		for x := this.MinX; x <= this.MaxX; x++ {
 			if val, ok := grid[[2]int{x, y}]; ok {
-				output = output + string(val)
+				output = output + string(val.plant)
 			} else {
 				output = output + "."
 			}
@@ -302,6 +230,6 @@ func PaintGrid(this *aocutils.Gridsize, grid map[Cell]rune) {
 		output = output + "\n"
 	}
 	fmt.Println(output)
-	time.Sleep(time.Millisecond * 20)
+	time.Sleep(time.Millisecond * 200)
 
 }
